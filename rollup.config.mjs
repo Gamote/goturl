39526e16cb58732bfbffb7cc61a7bc25a62caa46
Gamote <contatc@gamote.ro>
2023-12-18 15:44:07 +0200

    chore: people want both ESM and CSJ - give them both 🤷‍♂️

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -10,10 +10,20 @@ const external = ["url-regex-safe"]
 export default defineConfig([{
   input,
   external,
-  output: {
-    dir: dist,
-    format: 'es',
-  },
+  output: [
+    {
+      file: `${dist}/index.cjs.js`,
+      format: 'cjs',
+    },
+    {
+      file: `${dist}/index.esm.js`,
+      format: 'es',
+    }
+  ],
+  // output: {
+  //   dir: dist,
+  //   format: 'es',
+  // },
   plugins: [typescript(), terser()],
 }, {
   input,
